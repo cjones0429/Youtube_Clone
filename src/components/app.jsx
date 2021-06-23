5a9215d3463970cc3fb7f33070400d24ff8b1429
cjones0429 <cejones1997@aim.com>
2021-06-23 13:55:22 -0400

    made a search for video function using axios.get to googleapis. inserted strict videoId to test. will be string in the future. returned iframe. working code

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            videoId: '2RQh0BOoBbQ'
+         }
     }
+
+    searchForVideo = async (searchQuery) => {
+        let response = await axios.get(`http://www.googleapis.com/youtube/v3/search?q=${searchQuery}&key=AIzaSyC_TUmTIqQx4r4y8Y16DpOQNoxRkNuIkl4`)
+        let videos = response.date;
+        this.setState({
+            videoId: videos.items[0].id.videoId
+        })
+    }
+
+    componentDidMount(){
+        this.searchForVideo();
+    }
+
     render() { 
         return ( 
-            <h1>React App Using Component</h1>
+            <div>
+                <center>
+                    <h1>YouTube Clone</h1>
+                </center>
+                <br/>
+                <iframe id="ytplayer" type="text/html" width="640" height="360"
+                src={`https://www.youtube.com/embed/${this.state.videoId}?autoplay=1&origin=http://example.com`}
+                frameborder="0"></iframe>
+            </div>
          );
     }
 }
