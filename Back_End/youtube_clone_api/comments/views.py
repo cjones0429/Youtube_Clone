c4ab42caed36be1c5b05a9faa3e382e970017e85
unknown <mrchrisfarrell@yahoo.com>
2021-06-24 10:22:28 -0400

    added patch view for likes/dislikes

--- Back_End/youtube_clone_api/comments/views.py
+++ Back_End/youtube_clone_api/comments/views.py
@@ -22,3 +22,22 @@ class CommentList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def patch(self, request, pk):
+        comment = self.get_object(pk)
+        if request.data == "like":
+            comment.likes += 1
+        else:
+            comment.dislikes += 1
+        comment.save()
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
